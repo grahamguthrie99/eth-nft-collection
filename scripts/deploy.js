506333953fa67d1314fca9332382231362b1aa9e
grahamguthrie99 <grahamguthrie99@gmail.com>
2021-11-29 20:08:28 -0500

    Change function signature in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,15 +5,11 @@ const main = async () => {
     console.log("Contract deployed to:", nftContract.address);
   
     // Call the function.
-    let txn = await nftContract.makeAnEpicNFT()
+    let txn = await nftContract.makeLocationNFT()
     // Wait for it to be mined.
     await txn.wait()
     console.log("Minted NFT #1")
-  
-    txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-    console.log("Minted NFT #2")
+
   };
   
   const runMain = async () => {
